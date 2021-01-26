c35ec763e6d8cf41fdd0bab715bcef2ea1f9e3f0
vahnkaiser <guilhermemachado9@live.com.pt>
2021-01-26 14:24:24 +0000

    Initial commit

---
+++ adb_helper.py
@@ -0,0 +1,66 @@
+'''
+Created on Jan 22, 2021
+
+@author: Guilherme
+'''
+
+from tkinter import Tk, Button, filedialog
+from time import sleep
+from subprocess import run, Popen
+
+root = Tk()
+root.geometry("370x130")
+root.title("ADB Helper")
+
+def execute(command):
+    run(command, shell=True)
+
+def execute_p(command):
+    Popen(command)
+
+def create_button(text, width, position, command):
+    button = Button(text = text, width = width, command = command)
+    button.configure(bg = "#f0f0f0")
+    button.place(x = position[0], y = position[1])
+    return button
+
+def adb_connect_firetv():
+    execute("adb disconnect")
+    execute("adb connect 192.168.10.130:5555")
+    button_ftv.configure(bg = "#e8fbe8")
+    button_atv.configure(bg = "#f0f0f0")
+    
+
+def adb_connect_androidtv():
+    execute("adb disconnect")
+    execute("adb connect 192.168.10.101:5555")
+    button_ftv.configure(bg = "#f0f0f0")
+    button_atv.configure(bg = "#e8fbe8")
+    
+def display_device():
+    execute_p("scrcpy -m 1600")
+    
+def install_pkg():
+    my_filetypes = [('apk files', '.apk')]
+    path = filedialog.askopenfilename(filetypes = my_filetypes)
+    if path != "":
+        execute_p(f'adb install -r "{path}"')
+
+def adb_disconnect():
+    execute("adb disconnect")
+    button_ftv.configure(bg = "#f0f0f0")
+    button_atv.configure(bg = "#f0f0f0")
+
+# Not in use. Need to find a way to re scan the network through cmd.
+def connect_to_network(name):
+    execute(f"netsh wlan connect name={name}")
+    sleep(3)
+
+button_ftv = create_button("Connect to FireTV", 20, [10, 30], adb_connect_firetv)
+button_atv = create_button("Connect to AndroidTV", 20, [10, 70], adb_connect_androidtv)
+create_button("Display device", 20, [210, 10], display_device)
+create_button("Install .apk", 20, [210, 50], install_pkg)
+create_button("Disconnect", 20, [210, 90], adb_disconnect)
+
+root.resizable(False, False)
+root.mainloop()
---
+++ adb_helper.spec
@@ -0,0 +1,33 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['adb_helper.py'],
+             pathex=['C:\\Users\\Guilherme\\eclipse-workspace\\adb_helper'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          [],
+          name='adb_helper',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          upx_exclude=[],
+          runtime_tmpdir=None,
+          console=True )
