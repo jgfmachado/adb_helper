65ca80ea410405e9b706a39c0fc2b85a7973a4f7
vahnkaiser <guilhermemachado9@live.com.pt>
2021-11-15 20:39:42 +0000

    added config.ini
    - added config.ini to personalize the devices names and ips

--- adb_helper.py
+++ adb_helper.py
@@ -6,10 +6,19 @@ Created on Jan 22, 2021
 
 from tkinter import Tk, Button, filedialog, simpledialog
 from subprocess import run, Popen
+import configparser
 
 root = Tk()
 root.geometry("370x130")
 root.title("ADB Helper")
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+DEVICE_1_NAME = config['device_1']['name']
+DEVICE_1_IP = config['device_1']['ip']
+DEVICE_2_NAME = config['device_2']['name']
+DEVICE_2_IP = config['device_2']['ip']
 
 def execute(command):
     run(command, shell=True)
@@ -22,13 +31,13 @@ def create_button(text, width, position, command):
     button.place(x = position[0], y = position[1])
     return button
 
-def adb_connect_firetv():
+def adb_connect_device_1():
     execute("adb disconnect")
-    execute("adb connect 192.168.5.118:5555")
+    execute(f"adb connect {DEVICE_1_IP}:5555")
     
-def adb_connect_androidtv():
+def adb_connect_device_2():
     execute("adb disconnect")
-    execute("adb connect 192.168.5.101:5555")
+    execute(f"adb connect {DEVICE_2_IP}:5555")
 
 def adb_disconnect():
     execute("adb disconnect")
@@ -36,7 +45,7 @@ def adb_disconnect():
 def take_screenshot():
     file_name = simpledialog.askstring("", "Input a name for the screenshot.\
     \nIt shouldn't be empty nor include forbidden characters.\
-    \nForbidden characters:  <  >  :  \"  /  \\  |  ?  *")
+    \nForbidden characters:  spaces  <  >  :  \"  /  \\  |  ?  *")
     forbidden_chars = ['<','>',':','"','/','\\','|','?','*']
     if file_name != "" and file_name not in forbidden_chars:
         execute(f"adb exec-out screencap -p > {file_name}.png")
@@ -51,8 +60,8 @@ def install_pkg():
         execute_p(f'adb install "{path}"')
         
 
-create_button("Connect to FireTV", 20, [10, 10], adb_connect_firetv)
-create_button("Connect to AndroidTV", 20, [10, 50], adb_connect_androidtv)
+create_button(f"Connect to {DEVICE_1_NAME}", 20, [10, 10], adb_connect_device_1)
+create_button(f"Connect to {DEVICE_2_NAME}", 20, [10, 50], adb_connect_device_2)
 create_button("Disconnect", 20, [10, 90], adb_disconnect)
 create_button("Take screenshot", 20, [210, 10], take_screenshot)
 create_button("Display device", 20, [210, 50], display_device)
